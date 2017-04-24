e276ee6e8215c94a1a99e953b9e7fd42274a2acb
lzx2005 <978744976@qq.com>
2017-04-24 18:28:09 +0800

    编写主A

--- app/src/main/java/com/lzx2005/system/order/activity/LoginActivity.java
+++ app/src/main/java/com/lzx2005/system/order/activity/LoginActivity.java
@@ -1,12 +1,17 @@
 package com.lzx2005.system.order.activity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Handler;
 import android.os.Message;
+import android.support.design.widget.NavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -19,37 +24,53 @@ import com.lzx2005.system.order.http.task.LoginTask;
 
 
 public class LoginActivity extends AppCompatActivity {
-    TextView username;
-    TextView password;
-    Button loginSubmit;
-    LinearLayout linearLayout;
+    private TextView username;
+    private TextView password;
+    private Button loginSubmit;
+    private LinearLayout linearLayout;
+    private SharedPreferences loginInfo;
+    private ProgressDialog progressDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-        username = (TextView) findViewById(R.id.username);
-        password = (TextView) findViewById(R.id.password);
-        loginSubmit = (Button) findViewById(R.id.sign_in_button);
-        linearLayout = (LinearLayout)findViewById(R.id.username_login_form);
 
-        //loginSubmit.setEnabled(false);
-        loginSubmit.setOnClickListener(v ->{
-            if(TextUtils.isEmpty(username.getText())){
-                alert("请输入用户名");
-                //loginSubmit.setEnabled(false);
-                return;
-            }
-            if(TextUtils.isEmpty(password.getText())){
-                alert("请输入密码");
-                //loginSubmit.setEnabled(false);
-                return;
-            }
-            //alert(username.getText()+","+password.getText());
-            String host = getResources().getString(R.string.server_host);
-            String url = host + "/user/login?username="+username.getText()+"&password="+password.getText();
-            LoginTask loginTask = new LoginTask(url, handler);
-            new Thread(loginTask).start();
-        });
+        setContentView(R.layout.activity_login);
+
+
+        loginInfo = getSharedPreferences("loginInfo", 0);
+        String token = loginInfo.getString("token","no");
+        if(!token.equals("no")){
+            //已经登录
+            Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+            LoginActivity.this.startActivity(intent);
+        }else{
+
+            username = (TextView) findViewById(R.id.username);
+            password = (TextView) findViewById(R.id.password);
+            loginSubmit = (Button) findViewById(R.id.sign_in_button);
+            linearLayout = (LinearLayout)findViewById(R.id.username_login_form);
+
+            //loginSubmit.setEnabled(false);
+            loginSubmit.setOnClickListener(v ->{
+                if(TextUtils.isEmpty(username.getText())){
+                    alert("请输入用户名");
+                    //loginSubmit.setEnabled(false);
+                    return;
+                }
+                if(TextUtils.isEmpty(password.getText())){
+                    alert("请输入密码");
+                    //loginSubmit.setEnabled(false);
+                    return;
+                }
+                progressDialog = ProgressDialog.show(LoginActivity.this, "正在登录...", "请稍后...", true, false);
+                //alert(username.getText()+","+password.getText());
+                String host = getResources().getString(R.string.server_host);
+                String url = host + "/user/login?username="+username.getText()+"&password="+password.getText();
+                LoginTask loginTask = new LoginTask(url, handler);
+                new Thread(loginTask).start();
+            });
+        }
+
 
     }
 
@@ -62,7 +83,6 @@ public class LoginActivity extends AppCompatActivity {
             String val = data.getString("value");
             Log.e("lzx2005", val);
             //alert(val);
-            SharedPreferences loginInfo = getSharedPreferences("loginInfo", 0);
 
             JSONObject jsonObject = JSONObject.parseObject(val);
             if(jsonObject.getInteger("code")==0){
@@ -73,6 +93,15 @@ public class LoginActivity extends AppCompatActivity {
                     alert("登录成功！");
                     loginInfo.edit().putString("token",token).apply();
                     //跳转
+                    progressDialog.dismiss();
+
+                    NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+                    LinearLayout linearLayout = (LinearLayout)navigationView.getHeaderView(0);
+                    Menu menu = navigationView.getMenu();
+                    MenuItem item = menu.findItem(R.id.nav_exit);
+
+
+                    LoginActivity.this.finish();
                 }
             }else{
                 alert(jsonObject.getString("msg"));
---
+++ app/src/main/java/com/lzx2005/system/order/activity/MainActivity.java
@@ -0,0 +1,136 @@
+package com.lzx2005.system.order.activity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.util.Log;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.lzx2005.system.order.R;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    SharedPreferences loginInfo;
+    FloatingActionButton fab;
+    MenuItem exitMenuItem;
+
+    TextView usernameHead;
+    TextView userinfoHead;
+
+    ImageView userNoLoginImage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        loginInfo = getSharedPreferences("loginInfo", 0);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("知道了", v -> Log.e("lzx2005","1")).show());
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+
+        LinearLayout linearLayout = (LinearLayout)navigationView.getHeaderView(0);
+        Menu menu = navigationView.getMenu();
+        MenuItem item = menu.findItem(R.id.nav_exit);
+
+
+
+        String token = loginInfo.getString("token","no");
+        if(token.equals("no")){
+            //未登录
+            item.setVisible(false);
+            //exitMenuItem.setVisible(false);
+            linearLayout.setOnClickListener(v->{
+                Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+                MainActivity.this.startActivity(intent);
+            });
+        }else{
+            //usernameHead.setText("lzx2005");
+            //userinfoHead.setText("asdadwdawdaaaaaaaaaaaaa");
+        }
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_exit) {
+            loginInfo.edit().remove("token").apply();
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
