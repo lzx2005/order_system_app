dd2a4c3e6c5bf101c7e5c5175b0938b33a54bd85
lzx2005 <978744976@qq.com>
2017-04-26 14:27:22 +0800

    高德地图编辑

--- app/src/main/java/com/lzx2005/system/order/activity/MapActivity.java
+++ app/src/main/java/com/lzx2005/system/order/activity/MapActivity.java
@@ -3,9 +3,11 @@ package com.lzx2005.system.order.activity;
 import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.amap.api.maps.AMap;
 import com.amap.api.maps.MapView;
+import com.amap.api.maps.model.CameraPosition;
 import com.amap.api.maps.model.MyLocationStyle;
 import com.lzx2005.system.order.R;
 
@@ -21,7 +23,6 @@ public class MapActivity extends AppCompatActivity {
         mMapView = (MapView) findViewById(R.id.map);
         //在activity执行onCreate时执行mMapView.onCreate(savedInstanceState)，创建地图
         mMapView.onCreate(savedInstanceState);
-
         //获取地图控件引用
         setTitle("附近的餐厅");
 
@@ -29,11 +30,26 @@ public class MapActivity extends AppCompatActivity {
         AMap aMap = mMapView.getMap();
         MyLocationStyle myLocationStyle;
         myLocationStyle = new MyLocationStyle();//初始化定位蓝点样式类
-        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE);//连续定位、且将视角移动到地图中心点，定位点依照设备方向旋转，并且会跟随设备移动。（1秒1次定位）如果不设置myLocationType，默认也会执行此种模式。
+        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATE) ;//定位一次，且将视角移动到地图中心点。
         myLocationStyle.interval(2000); //设置连续定位模式下的定位间隔，只在连续定位模式下生效，单次定位模式下不会生效。单位为毫秒。
         aMap.setMyLocationStyle(myLocationStyle);//设置定位蓝点的Style
         aMap.getUiSettings().setMyLocationButtonEnabled(true);//设置默认定位按钮是否显示，非必需设置。
+        aMap.getUiSettings().setZoomPosition(18);
         aMap.setMyLocationEnabled(true);// 设置为true表示启动显示定位蓝点，false表示隐藏定位蓝点并不进行定位，默认是false。
+
+        aMap.setOnCameraChangeListener(new AMap.OnCameraChangeListener(){
+
+            @Override
+            public void onCameraChange(CameraPosition cameraPosition) {
+                //Log.i("lzx2005","camera开始移动了"+cameraPosition.toString());
+            }
+
+            @Override
+            public void onCameraChangeFinish(CameraPosition cameraPosition) {
+                //todo 去服务器获取附近的餐馆
+                Log.i("lzx2005","camera结束移动了"+cameraPosition.toString());
+            }
+        });
     }
 
     @Override
